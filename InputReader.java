1e23eee18f58c37e12b968303eba63a808de48b8
Unknown <michael.galczewski@gmail.com>
2016-11-05 01:25:57 +1100

    Updated Java Template nextLine() method.

--- InputReader.java
+++ InputReader.java
@@ -26,6 +26,17 @@ public class InputReader {
         return true;
     }
 
+    public String nextLine() {
+        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            try {
+                tokenizer = new StringTokenizer(reader.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return tokenizer.nextToken("\n");
+    }
+
     public String next() {
         while (tokenizer == null || !tokenizer.hasMoreTokens()) {
             try {
